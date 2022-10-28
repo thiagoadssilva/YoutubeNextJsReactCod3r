ed4924b0409fd95f451ac14179ac84419909f36d
thiago jose da silva <thiago.ads.silva@gmail.com>
2022-10-28 05:06:13 -0300

    Finalização do componente botao

--- next-crud/src/components/Tabela.tsx
+++ next-crud/src/components/Tabela.tsx
@@ -39,9 +39,9 @@ export default function Tabela(props: TabelaProps){
 
     function resderizarAcoes(cliente: Cliente){
         return(
-            <td className='flex'>
+            <td className='flex justify-center' >
                 {props.clienteSelecionado ? (
-                    <button className='
+                    <button onClick={() => props.clienteSelecionado?.(cliente)} className='
                     flex justify-center items-center
                     text-green-600 rounded-full p-2 m-1
                     hover:bg-purple-50
@@ -51,7 +51,7 @@ export default function Tabela(props: TabelaProps){
                 ): false}
 
                 {props.clienteExcluido ? (
-                    <button className='
+                    <button onClick={() => props.clienteExcluido?.(cliente)} className='
                         flex justify-center items-center
                         text-red-600 rounded-full p-2 m-1
                         hover:bg-purple-50
--- next-crud/src/pages/index.jsx
+++ next-crud/src/pages/index.jsx
@@ -1,6 +1,7 @@
 import Layout from '../components/Layout'
 import Tabela from '../components/Tabela'
 import Cliente from '../core/Cliente'
+import Botao from '../components/Botao'
 
 const clientes = [
   new Cliente('Thiago', 36, '1'),
@@ -8,16 +9,34 @@ const clientes = [
   new Cliente('Bento', 5, '3'),
 ]
 
-function clienteSelecionado(cliente: Cliente){
-  return '123'
+function clienteSelecionado(clientes){
+  console.log(clientes.nome)
+}
+
+function clienteExcluido(clientes){
+  console.log("excluido" + clientes.nome)
 }
 
 export default function Home() {
-  return (
-    <div className={`flex justify-center items-center h-screen bg-gradient-to-r from-blue-500 to-purple-500 text-white`}>
+  return ( 
+    <div className={`
+      flex 
+      justify-center 
+      items-center 
+      h-screen 
+      bg-gradient-to-r from-blue-500 to-purple-500 
+    text-white`
+    }>
       <Layout titulo="Cadastro Simples">
-        <Tabela clientes={clientes}
-        clienteSelecionado={clienteSelecionado}></Tabela>
+        <div className='flex justify-end'>
+          <Botao cor="green" className="mb-4">Novo Cliente</Botao>
+        </div>
+        
+        <Tabela 
+          clientes={clientes}
+          clienteSelecionado={clienteSelecionado}
+          clienteExcluido={clienteExcluido}
+        ></Tabela>
       </Layout> 
     </div>
   )
